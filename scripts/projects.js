1283a5ddeb71bcd2155efb5ec86e2b3726b921a4
Aaron Meier <aamea72@student.oaisd.org>
2025-02-14 10:14:31 -0500

    Tried to change project icons

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,14 +1,14 @@
 const myProjects = [
     {
         name: "Project 1",
-        icon: "./assets/icons/languages/lua.svg"
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29_tU-a7XD5hngSIeJL2DzXd4MTWdjLa7HA&s"
     },
     {
         name: "Project 2",
-        icon: "./assets/icons/languages/lua.svg"
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29_tU-a7XD5hngSIeJL2DzXd4MTWdjLa7HA&s"
     },
     {
         name: "Project 3",
-        icon: "./assets/icons/languages/lua.svg"
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29_tU-a7XD5hngSIeJL2DzXd4MTWdjLa7HA&s"
     },
 ]
